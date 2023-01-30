6263774be76f2dd45f3f1b994fd31e22d8dffb65
jhfranco2 <jhoanmateofranco@gmail.com>
2023-01-30 14:53:36 -0500

    añadiendo las regiones en la clase service y controller

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/springbootbackendapirest/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/springbootbackendapirest/models/entity/Cliente.java
@@ -3,12 +3,18 @@ package com.bolsadeideas.springboot.backend.apirest.springbootbackendapirest.mod
 import java.io.Serializable;
 import java.util.Date;
 
+import org.hibernate.annotations.Fetch;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -51,6 +57,11 @@ public class Cliente implements Serializable {
     private Date createAt;
 
     private String foto;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+    private Region region;
     
     private static final long serialVersionUID = 1L;
 }
---
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/springbootbackendapirest/models/entity/Region.java
@@ -0,0 +1,23 @@
+package com.bolsadeideas.springboot.backend.apirest.springbootbackendapirest.models.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "regiones")
+public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+}
